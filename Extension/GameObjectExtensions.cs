41e31e92102689384e93ec044be8cb40a281ad04
Momin Khan <foolmoron@gmail.com>
2018-04-17 23:03:41 -0400

    more

--- Extension/GameObjectExtensions.cs
+++ Extension/GameObjectExtensions.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public static class GameObjectExtensions {
 
-    public static T GetComponentInSelfOrChildren<T>(this GameObject gameObject) where T : Behaviour {
+    public static T GetComponentInSelfOrChildren<T>(this GameObject gameObject) where T : Component {
         var t = gameObject.GetComponent<T>();
         if (!t) {
             t = gameObject.GetComponentInChildren<T>();
+        }
+        return t;
+    }
+
+    public static T GetComponentInSelfOrChildren<T>(this Component component) where T : Component {
+        var t = component.GetComponent<T>();
+        if (!t) {
+            t = component.GetComponentInChildren<T>();
         }
         return t;
     }
